54d1275ff2b9babf6e10d0d02234b173be026ac8
Alexander Kjeldaas <ak@formalprivacy.com>
2014-04-13 00:16:19 +0200

    Not patching elf binaries result in non-deterministic builds.
    I have no idea why dontPatchELF was set.  If it was not important enough to comment
    on it might have been a spurious issue.
    
    (cherry picked from commit 064b825dde64dad757a30c740bdbcc07d1bda175)

--- pkgs/development/libraries/openldap/default.nix
+++ pkgs/development/libraries/openldap/default.nix
@@ -15,8 +15,6 @@ stdenv.mkDerivation rec {
     ] ++ stdenv.lib.optional (openssl == null) "--without-tls"
       ++ stdenv.lib.optional (cyrus_sasl == null) "--without-cyrus-sasl";
 
-  dontPatchELF = 1; # !!!
-
   meta = {
     homepage = "http://www.openldap.org/";
     description = "An open source implementation of the Lightweight Directory Access Protocol";
